4d5e9822d0e03326fcb23e7d7fbe65bb8d961b83
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2023-11-14 18:23:43 +0100

    Ajout de la page d'accueil

--- views.py
+++ views.py
@@ -5,5 +5,6 @@ from .app import app
 @app.route('/')
 def accueil():
     return render_template(
-        "accueil.html"
+        "accueil.html",
+        billets = [(0,"Nom du billet", "Description du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est good ou non ?", "18/03/2024", "Saint Jacques le moine", "$50000"),(1,"Nom du billet2", "Description du billet2", "18/03/2025", "Saint Jacques le moine2", "$100")]
     )

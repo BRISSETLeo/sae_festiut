a3c42f0a5e4a8af441c30f57919b620d2292eb76
Sevellec Maxime <o22200457@info03-31.iut45.univ-orleans.fr>
2023-11-16 13:46:35 +0100

    ajout des fichier pour la creation bd

--- views.py
+++ views.py
@@ -8,3 +8,9 @@ def accueil():
         "accueil.html",
         billets = [(0,"Nom du billet", "Description du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est goodDescription du billet admettons le texte est un peu trop grand pour cette page à quel moment je pourrais voir si c'est good ou non ?", "18/03/2024", "Saint Jacques le moine", "$50000"),(1,"Nom du billet2", "Description du billet2", "18/03/2025", "Saint Jacques le moine2", "$100")]
     )
+
+@app.route('/connexion')
+def connexion():
+    return render_template(
+        "connexion.html"
+    )
